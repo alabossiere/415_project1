55fbf2087b14698b83435a0ed27003e0e5342971
alabossiere <labossie@sonoma.edu>
2018-09-27 19:49:44 -0700

    Update integerchecking.py

--- integerchecking.py
+++ integerchecking.py
@@ -4,9 +4,24 @@
 #        return t
 #    t ← t - 1
 
-def intcheck():
+# function for consecutive integer checking algorithm
+def intCheck():
   t = min{m, n}
   while (t > 0) do
     if(m % t == 0 and n % t == 0)
         return t
+    t = t - 1
+
+    
+    COULD WE USE THE BOTTOM????
+    
+# function for consecutive integer checking algorithm and getting modulo average
+def intCheck(m, n):
+  dcount = 0;
+  t = min{m, n}
+  while (t > 0) do
+    if(m % t == 0 and n % t == 0)
+      dcount += 2
+      return t
+    dcount += 1
     t = t - 1
